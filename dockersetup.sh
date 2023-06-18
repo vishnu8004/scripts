88a8549e69fdbe3cb5f78f65050b1940109ce708
vishnu8004 <vishnusmailbox04@gmail.com>
2023-06-18 22:34:34 +0530

    fun

--- dockersetup.sh
+++ dockersetup.sh
@@ -4,6 +4,7 @@
 # Copyright (c) vewris.com
 # This scripts sets up docker repository to your linux desktop and installs docker engine
 
+echo "Entha bro modenoo!!!!!!!!"
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
