f3cfc0f7ff6e8e622411c7e23dc19f8ea3c9bf18
vishnu8004 <vishnusmailbox04@gmail.com>
2023-06-18 22:45:00 +0530

    EDIT FOR LINUX

--- dockersetup.sh
+++ dockersetup.sh
@@ -17,7 +17,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # The following command to sets up the repository:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker Engine
